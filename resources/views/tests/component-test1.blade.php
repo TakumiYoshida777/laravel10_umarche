63289d8002a5c27a41f67d0c0bbfc0a67c499d2e
takumi <yoshida.fm.0626@gmail.com>
2023-11-11 08:10:52 +0900

    変数

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -4,7 +4,7 @@
     </x-slot>
     コンポーネントテスト1
 
-    <x-tests.card title="タイトル" content="本文">
+    <x-tests.card title="タイトル" content="本文" :message="$message">
 
     </x-tests.card>
 </x-tests.app>
