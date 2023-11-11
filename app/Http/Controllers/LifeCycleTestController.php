416d131f85520e57dac65ea96c818c64f961afc8
takumi <yoshida.fm.0626@gmail.com>
2023-11-12 08:06:25 +0900

    サービスコンテナ

---
+++ app/Http/Controllers/LifeCycleTestController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LifeCycleTestController extends Controller
+{
+    public function showServiceContainerTest()
+    {
+        // サービスコンテナにコンテナに登録する
+        app()->bind('lifeCycleTest', function () {
+            return "ライフサイクルテスト";
+        });
+
+        // サービスコンテナから取得する
+        $test = app()->make('lifeCycleTest');
+
+        //サービスコンテナなしのパターン
+        $message = new Message();
+        $sample = new Sample($message);
+        $sample->run();
+
+        // サービスコンテナありのパターン
+        app()->bind('sample', Sample::class);
+        $sample = app()->make('sample');
+        $sample->run();
+
+        dd($test, app());
+    }
+}
+
+class Sample
+{
+    public $message;
+    public function __construct(Message $message)
+    {
+        $this->message = $message;
+    }
+
+    public function run()
+    {
+        $this->message->send();
+    }
+}
+
+
+class Message
+{
+    public function send()
+    {
+        echo "メッセージ表示";
+    }
+}
