c47a13492ffd2942c9e3558a3afcd9b40289f001
takumi <yoshida.fm.0626@gmail.com>
2023-11-18 06:03:50 +0900

    edit update

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -61,7 +61,7 @@
                                                 class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
                                             </th>
                                             <th
-                                                class="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br">
+                                                class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br">
                                             </th>
                                         </tr>
                                     </thead>
@@ -72,8 +72,9 @@
                                             <td class="px-4 py-3">{{ $owner->email }}</td>
                                             <td class="px-4 py-3">{{$owner->created_at->diffForHumans();}}
                                             </td>
-                                            <td class="w-10 text-center">
-                                                <input name="plan" type="radio">
+                                            <td class="px-4 py-3">
+                                                <button type="submit" onclick="location.href='{{ route('admin.owners.edit', ['owner' => $owner->id]) }}'"
+                                                class="text-white bg-indigo-500 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-600 rounded text-lg">更新する</button>
                                             </td>
                                         </tr>
                                         @endforeach
